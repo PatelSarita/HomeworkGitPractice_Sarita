ef0c8e36669bee9cce6d45655f6975c65d33663e
Sarita <patelsarita@gmail.com>
2024-03-25 13:54:41 +0100

    StagestOfGit file updated

--- src/StagesOfGit.java
+++ src/StagesOfGit.java
@@ -1,7 +1,7 @@
 public class StagesOfGit {
     public static void main(String[] args) {
-        System.out.println("1. Modified");
-        System.out.println("2. Staged");
-        System.out.println("3. Committed");
+        System.out.println("1. Modified :means that you have changed the file but have not committed it to your database yet.");
+        System.out.println("2. Staged :means that you have marked a modified file in its current version to go into your next commit");
+        System.out.println("3. Committed :means that the data is safely stored in your local database.");
     }
 }
