f27beb6fc8af07f0fbf4ddc5439ad3604db364e4
Sarita <patelsarita@gmail.com>
2024-03-25 14:37:59 +0100

    Command file uploaded

---
+++ src/Command.java
@@ -0,0 +1,7 @@
+public class Command {
+    public static void main(String[] args) {
+        System.out.println("git init : command to initialize a Git repository");
+        System.out.println("git add : command to add files to staging area");
+        System.out.println("git commit -m'your massage' : command to commit files to Git repository with message.");
+    }
+}
