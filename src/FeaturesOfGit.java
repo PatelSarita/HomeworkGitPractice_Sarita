cd4b5c29b17f339097b177db37c93082ff23dd1a
Sarita <patelsarita@gmail.com>
2024-03-25 14:39:11 +0100

    FeaturesOfGit file updated

--- src/FeaturesOfGit.java
+++ src/FeaturesOfGit.java
@@ -3,5 +3,7 @@ public class FeaturesOfGit {
         System.out.println("1. Free and open source.");
         System.out.println("2. Speed: It completes all the tasks really fast.");
         System.out.println("3. Scalable: The number of collaborators increase Git can easily handle this change;");
+        System.out.println("4. Easy branching ");
+        System.out.println("5. Economical");
     }
 }
