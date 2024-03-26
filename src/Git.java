fe955c41773e65a57b27d7e79905472c680c5d8c
PatelSarita <164254624+PatelSarita@users.noreply.github.com>
2024-03-26 18:19:40 +0100

    Update Git.java

--- src/Git.java
+++ src/Git.java
@@ -2,5 +2,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Git is a DevOps tool used for source code management.It is a free  and open-source version control system used to handle smmall to very large projects efficiently.");
         System.out.println("Git is used to tracking changes in the source code,enabling multiple developers to work together on linear development.");
+        System.out.println("Hello World!!!");
+        
     }
 }
